f5fcc23ba1e8f40b7e892d3d33eb4a35a1b68515
Rich Barton <rbarton@corsis.com>
2016-07-01 23:07:43 -0400

    add personal app

--- espnff_analyzer/espnff_analyzer/settings.py
+++ espnff_analyzer/espnff_analyzer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 	'powerrankings',
+    'personal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- espnff_analyzer/espnff_analyzer/urls.py
+++ espnff_analyzer/espnff_analyzer/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rankings/', include('powerrankings.urls')),	
+    url(r'^rankings/', include('powerrankings.urls')),
+    url(r'^$', include('personal.urls')),
 ]
